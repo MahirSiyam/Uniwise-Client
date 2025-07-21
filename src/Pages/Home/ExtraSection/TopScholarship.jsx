e5fb5f5c7ff9bce3c4b2fc111cd3b44cecae2056
Mahir Siyam <mahirmubtasimsiyam@gmail.com>
2025-07-21 18:07:07 +0600

    added top scholarship

--- src/Pages/Home/ExtraSection/TopScholarship.jsx
+++ src/Pages/Home/ExtraSection/TopScholarship.jsx
@@ -21,17 +21,31 @@ const TopScholarship = () => {
       <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
         {scholarships.map((item) => (
           <div key={item._id} className="card bg-base-300 shadow-xl">
-            {/* <figure> */}
-              <img src={item.universityImage} alt={item.universityName} className="h-[400px] w-[550px] object-cover rounded-t-lg" />
-            {/* </figure> */}
+            <img
+              src={item.universityImage}
+              alt={item.universityName}
+              className="h-[400px] w-[550px] object-cover rounded-t-lg"
+            />
             <div className="card-body">
               <h2 className="card-title">{item.universityName}</h2>
-              <p><strong>Degree:</strong> {item.degree}</p>
-              <p><strong>Subject:</strong> {item.subjectCategory}</p>
-              <p><strong>Fees:</strong> ${item.applicationFees}</p>
-              <p className="text-sm"><strong>Posted:</strong> {new Date(item.postDate).toDateString()}</p>
+              <p>
+                <strong>Degree:</strong> {item.degree}
+              </p>
+              <p>
+                <strong>Subject:</strong> {item.subjectCategory}
+              </p>
+              <p>
+                <strong>Fees:</strong> ${item.applicationFees}
+              </p>
+              <p className="text-sm">
+                <strong>Posted:</strong>{" "}
+                {new Date(item.postDate).toDateString()}
+              </p>
               <div className="card-actions justify-end">
-                <Link to={`/scholarship/${item._id}`} className="btn bg-red-600 hover:bg-red-700 btn-sm text-white">
+                <Link
+                  to={`/scholarship/${item._id}`}
+                  className="btn bg-red-600 hover:bg-red-700 btn-sm text-white"
+                >
                   Details
                 </Link>
               </div>
@@ -40,8 +54,11 @@ const TopScholarship = () => {
         ))}
       </div>
 
-      <div className="text-center mt-12"> 
-        <Link to="/allScholarship" className="btn bg-red-600 hover:bg-red-700 btn-md text-white">
+      <div className="text-center mt-12">
+        <Link
+          to="/allScholarship"
+          className="btn bg-red-600 hover:bg-red-700 btn-md text-white"
+        >
           View All Scholarships
         </Link>
       </div>
